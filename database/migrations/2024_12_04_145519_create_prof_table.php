3e033832d84f700c7fc4f677c90fd69234309b98
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2025-04-08 18:20:50 +0100

    Adding the download repository link to an searched repo (not working on the moment)

--- database/migrations/2024_12_04_145519_create_prof_table.php
+++ database/migrations/2024_12_04_145519_create_prof_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfiles1Table extends Migration
+class CreateProfTable extends Migration
 {
     public function up()
     {
